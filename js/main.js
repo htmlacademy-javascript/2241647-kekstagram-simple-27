f325b021871cfc9c39e9c039a96f89aed21e5c57
kekthor <junglousdave@gmail.com>
2022-10-19 23:31:05 +0300

    Написал функции рандомизатора и проверки длины строки

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/*
+Функция, возвращающая случайное целое число из переданного диапазона включительно.
+имя_функции(от, до); // Результат: целое число из диапазона "от...до"
+*/
+function getRandomNumber (min, max) {
+  if (
+    Number.isInteger(min) &&
+    Number.isInteger(max) &&
+    min >=0 &&
+    max >= 0
+  ) {
+    if (min > max) {
+      let flip = min;
+      min = max;
+      max = flip;
+    }
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return NaN
+}
+
+
+/*
+Функция для проверки максимальной длины строки.
+имя_функции(проверяемая_строка, максимальная_длина); // Результат: true, если строка проходит по длине, и false — если не проходит
+*/
+function checkStringLength (string, maxLength) {
+  return string.length <= maxLength;
+}
