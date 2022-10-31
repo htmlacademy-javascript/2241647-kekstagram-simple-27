fc94871fc550faf1ddf03351f9fa2d30e36b26b6
Keks <keks@htmlacademy.ru>
2022-10-31 19:08:50 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-alert */
+
+import {start} from './data.js';
+
+alert(`У первой фотки из ${start.length} загруженных ${start[0].likes} лайков и ${start[0].comments} комментариев`);
