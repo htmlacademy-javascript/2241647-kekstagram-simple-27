0f14a77de23aa65e6806e42636428010988cac75
kekthor <junglousdave@gmail.com>
2022-11-15 19:07:16 +0300

    модуль 7

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
-/* eslint-disable no-alert */
-
 import './thumbnails.js';
-
-import {start} from './data.js';
-
-//alert(`У первой фотки из ${start.length} загруженных ${start[0].likes} лайков и ${start[0].comments} комментариев`);
-
-
-//alert('EEE33!');
